ed88d8af404893daa980ddcc952f4e5dc0f60710
Flint-cyber <61284000+Flint-cyber@users.noreply.github.com>
2020-03-12 17:06:04 -0600

    Imprime todos los números primos en un intervalo dado

--- Primos2.c
+++ Primos2.c
@@ -22,72 +22,83 @@ int main()
 	
 //Escaneamos la cota inferior, para darle a nuestro proceso, un número a partir del cuál empezar a calcular.
 	printf("Por favor, ingrese una cota inferior\n");
-	scanf("%d", &inf); 
+	scanf("%d", &inf); //Escaneamos el valor, y se lo asignamos a la variable inferior.
 
-	while(inf<=1)//No queremos ni el uno, ni el cero, ni números negativos en el programa, y con este WHILE nos aseguramos de que el usuario solo ingrese positivos mayores a uno.
+	while(inf<=1)//No queremos ni el uno, ni el cero, ni números negativos en el programa, y con este WHILE,
+			//nos aseguramos de que el usuario solo ingrese positivos mayores a uno.
 	{
 		printf("El número que ingresó no es válido.\n\nPor favor, ingrese una cota inferior\n");
 		scanf("%d", &inf);//Seguirá escaneando hasta que el valor sea mayor a uno.
 	}
 
 
-//Ahora es el turno de escanear una cota superior que nos diga cuándo terminar el proceso.
+//Ahora es el turno de escanear una cota superior que nos diga dónde terminar el proceso.
 	printf("Por favor, ingrese una cota superior\n"); 
-	scanf("%d", &sup);
+	scanf("%d", &sup); //Escaneamos el valor, y se lo asignamos a la variable superior.
 
-	while(sup<=inf)//No tiene sentido hablar de una cota Superior que sea menor a la Inferior, así que en caso de que se ingrese algo así, se pedirá al usuario que ingrese un dato distinto.
+	while(sup<=inf)//No tiene sentido hablar de una cota Superior que sea menor a la Inferior, 
+			//así que en caso de que se ingrese algo así, se pedirá al usuario que ingrese un dato distinto.
 	{	
-		if(sup==inf)//También evitamos que las dos cotas sean iguales, pues en dado caso, el programa ya no tiene chiste.
+		if(sup==inf)//Aquí evitamos que las dos cotas sean iguales, pues en dado caso, el programa ya no tiene chiste.
 		{
 			printf("Las cotas superior e inferior no pueden ser las mismas.\n\nPor favor, ingrese una cota superior\n");
 		}
 	
-		else if(sup<=1)//En caso de que el número cero, uno, o negativo, se notificará que el número no es válido.
+		else if(sup<=1)//Aquí, evitamosque el número cero, uno, o negativo.
 		{
 			printf("El número que ingresó no es válido.\n\nPor favor, ingrese una cota superior.\n");
 		}
 
-		else //Por último, tampoco podemos permitir que la cota superior sea menor a la inferior.
+		else //Por último, aqúi, en cualquier otro caso, solo impedimos que la cota superior sea menor a la inferior.
 		{
 			printf("La cota superior no puede ser menor a la inferior.\n\nPor favor, ingrese una cota superior\n");
 		}
 
 
-		scanf("%d", &sup);//Seguimos escaneando la cota superior hasta que se cumplan los requisitos.
+		scanf("%d", &sup);//Seguimos escaneando la cota superior hasta que se cumplan todos los requisitos.
 	}
 
 
-//Imprimimos un pequeño texto para que el usuario distinga la respuesta.
+//Imprimimos un pequeño texto y generamos una nueva línea, para que el usuario distinga la respuesta más fácilmente
 	printf("\nLos números primos en el intervalo [%d,%d] son:\n", inf, sup);
+	
+//Escribimos [] para dar a entender que las cotas inferior y superiro, de ser primas, irán incluidas en la lista.
+//Esto ya que es un intervalo cerrado.
 
 
 //Terminadas nuestras condicionales para los datos ingresados por el usuario, ya podemos comenzar el proceso.
 
 
-	while(inf <= sup) //Iremos analizando cada número desde la cota inferior hasta la superior, pero como el intervalo es cerrado, también debemos incluir las dos cotas en la lista.
+	while(inf <= sup) 
+//Iremos analizando cada número desde la cota inferior hasta la superior, pero como el intervalo es cerrado, 
+//también debemos incluir las dos cotas en la lista.
 
 	{
-		int Divisible = 0; //Nuestra flag para el ciclo FOR.
+		int Divisible = 0; //Nuestra flag para el ciclo FOR, la divisibilidad del número, empieza en cero.
+				   //Esto porque comenzamos asumiendo que el número es primo.
 
-		for(int x = 2; x < inf; x++)//La verificación empieza, por supuesto, desde el número dos, pero debe terminar en la cota inferior para verificar si es primo o no.
+		//Dentro del While, metemos el proceso de verificación de primos que habíamos realizado previamente: Primos1.c
+		
+		for(int x = 2; x < inf; x++) //Comenzamos el conteo en 2 pues todos los números son divisibles en 1.
+
 		{
-			if(inf%x ==0)
+			if(inf%x ==0) //Verificamos si el residuo es cero.
 			{
-				Divisible = 1;
+				Divisible = 1; //Si al menos uno de ellos es cero, se activa la Flag que indica que es divisible.
 			}
 		}
 
 		if(Divisible == 0) 
 		{
-			printf("%d\n", inf);
+			printf("%d\n", inf); //Si en ningún momento existe divisibilidad, entonces el número analizado será impreso.
+					     //En caso contrario, si hay divisibilidad, será ignorado.
 		}
 	
-		inf++; //Si no es divisible, entonces el número será añadido a la lista, y el ciclo WHILE pasará a analizar el siguiente número en la sucesión, hasta llegar a la cota superior.
+		inf++; //Luego de haber analizado la divisibilidad y luego de haberse impreso o ignorado el número,
+			//la cota inferior aumentará en uno, nuestro ciclo WHILE pasará a analizar el siguiente número en la sucesión, 
+			//y seguirá así hasta llegar a la cota superior.
 	}
 	
-
-
-
 return 0;
 }
 

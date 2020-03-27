398920af050a315fd1084c5744ccdef0d706e915
Flint-cyber <61284000+Flint-cyber@users.noreply.github.com>
2020-03-26 19:52:13 -0600

    Programa que pide al usuario las componentes de 2 vectores, y procede a realizar 4 operaciones con ellos

--- Vectores.c
+++ Vectores.c
@@ -9,7 +9,6 @@ Entrada:  x1, x2, y1, y2, z1, z2
 Salida:  Mag, Sum, Punto, Cruz
 */
 
-//Incluir librerias
 #include <stdio.h>
 #include <math.h>
 
@@ -20,15 +19,15 @@ float main ()
 	float Suma = 0;//Una variable de control para realizar nuestras operaciones, las cuales requieren cierto grado de recursividad.
 	float Vec1[3]; //El array del primer vector
 	float Vec2[3]; //El array del segundo vector
-	int i;
+	int i; //Variable de control para ciclos for
 
 //Comenzamos escaneando las componentes de cada vector
 
-	printf("Por favor, ingrese las coordenadas del Vector 1.\n");
+	printf("Por favor, ingrese las coordenadas del Vector 1.\n"); //Escameamos con un ciclo for, ya que es más conveniente que ir componente por componente.
 	
 	for(i = 0; i < 3; i++)
 	{
-		scanf("%f", &Vec1[i]);
+		scanf("%f", &Vec1[i]); //Iremos escaneando y asignando el valor a la i-ésima componente del vector, desde la cero-ésima hasta la segunda.
 	}
 
 
@@ -37,32 +36,26 @@ float main ()
 
 	for(i = 0; i < 3; i++)
 	{
-		scanf("%f", &Vec2[i]);
+		scanf("%f", &Vec2[i]); //Hacemos exactamente lo mismo que con el vector 1.
 	}
 
 
 	
 			//PROBLEMA UNO: MAGNITUDES
 
-	for (int i = 0; i < 3; i++)
+	for (int i = 0; i < 3; i++) //Con el loop, vamos analizando ordenadamente cada componente, y realizando la operación que corresponde.
 	{
-	
-//Con este loop, multiplicamos cada componente consigo misma de manera ordenada, y las vamos sumando poco a poco.
+		Prod = Vec1[i]*Vec1[i]; //Multiplicamos componente con componente.
 
-		Prod = Vec1[i]*Vec1[i];
+		//La variable Suma empieza en cero, y luego va añadiendo el cuadrado de cada componente.
 
-//La variable Suma empieza en cero para que nuestro for comience con el cuadrado de la primera componente, y luego vaya sumando más cuadrados hasta llegar a la tercera componente.
-
-		Suma = Suma + Prod;
+		Suma = Suma + Prod; //La variable suma ahora tomará el valor de ella misma, más el nuevo resultado del producto.
 	}
 
-//Saliendo del loop for, procedemos a sacar raíz cuadrada y asignamos ese valor a la variable Mag1, declarada anteriormente. 
+		Mag1 = sqrt(Suma); //Saliendo del loop for, procedemos a sacar raíz cuadrada y asignamos ese valor a la variable Mag1, declarada anteriormente. 
 
-		Mag1 = sqrt(Suma);
+	printf("\nNorma del Vector 1: %f\n", Mag1); //Imprimimos la Magnitud.
 
-//Imprimimos la Magnitud.
-
-	printf("\nNorma del Vector 1: %f\n", Mag1);
 		
 		Suma = 0; //Reseteamos Suma, para utilizarla en el siguiente loop.
 
@@ -71,49 +64,44 @@ float main ()
 
 	for (int i = 0; i < 3; i++)
 	{
-		Prod = Vec2[i]*Vec2[i];
-		Suma = Suma + Prod;
+		Prod = Vec2[i]*Vec2[i]; //Multiplicamos componente con componente.
+		Suma = Suma + Prod; //Suma toma el valor de ella misma en la iteración anterior, más el valor del producto.
 	}
 		
-		Mag2 = sqrt(Suma);
+		Mag2 = sqrt(Suma); //Sacamos raíz cuadrada.
 
-	printf("Norma del Vector 2: %f\n", Mag2);
+	printf("Norma del Vector 2: %f\n", Mag2); //Imprimimos.
 
-		Suma = 0;
+		Suma = 0; //Reseteamos el valor de Suma.
 
 
 
 		//PROBLEMA DOS: SUMA VECTORIAL
 
-	printf("\nResultado de la Suma Vectorial:\n");
-
-//Hacemos uso de un loop for que va a imprimir la suma de dos números que pertenezcan a la misma componente, desde la componente uno, hasta la tres.
-
-	for(int i = 0; i < 3; i++)
+	printf("\nResultado de la Suma Vectorial:\n"); //Imprimimos el encabezado
+	
+	for(int i = 0; i < 3; i++) //Creamos otro loop for.
 	{
-		Suma = Vec1[i] + Vec2[i];
+		Suma = Vec1[i] + Vec2[i]; //En cada iteración, sumamos componente con componente, del vector1 y el vector2.
 
-		printf("%f\n", Suma);
+		printf("%f\n", Suma); //Impriimos cada una de las sumas independientemente, pues son componentes ortogonales del vector nuevo.
 	}
-		Suma = 0;
-		
+		Suma = 0; //Reseteamos la variable Suma.		
 
+	
 
 		//PROBLEMA TRES: PRODUCTO ESCALAR
 
-	for (int i = 0; i < 3; i++)
-	{
-
 //Aquí, haremos el mismo procedimiento que con la norma, multiplicando componente con componente, pero de distintos vectores.
 
-		Prod = Vec1[i]*Vec2[i];
+	for (int i = 0; i < 3; i++) //Hacemos un loop for para el proceso iterativo.
+	{
+		Prod = Vec1[i]*Vec2[i]; //En cada iteración, multiplicamos la i-ésima componente del vector1, con la del vector2.
 
-//Luego, sumamos los resultados de las multiplicaciones. Aségurandonos de que SUma ya ha sido reseteado para la fluidez de la recursividad.
-
-		Suma = Suma + Prod;
+		Suma = Suma + Prod; //Suma toma el valor de ella misma en la iteración pasada, y le suma el valor del producto.
 	}
 
-//Por último, imprimimos, y reseteamos Suma.
+		//Por último, imprimimos el resultado, y reseteamos Suma.
 
 	printf("\nResultado del Producto Punto: %f\n", Suma);
 
@@ -125,30 +113,21 @@ float main ()
 		
 //Este es el problema más difícil de realizar, debido a lo laborioso que resulta el cálculo del producto vectorial.
 
-//Definimos nuestro tercer vector.
-		
-	float Vec3[3];
+	float Vec3[3]; //Definimos nuestro tercer vector.
 	
-//A continuación, le asignamos valores a cada componente de Vec3, basándonos en la fórmula para encontrar el producto cruz.
+//A continuación, le asignamos valores a cada componente de Vec3, basándonos en la fórmula para encontrar el producto cruz, la cual usa el determinante de una matriz de tres por tres.
 
 	Vec3[0] = Vec1[1]*Vec2[2] - Vec1[2]*Vec2[1];
 	Vec3[1] = Vec1[2]*Vec2[0] - Vec1[0]*Vec2[2];
 	Vec3[2] = Vec1[0]*Vec2[1] - Vec1[1]*Vec2[0];
 
+	printf ("\nResultado del Producto Cruz: \n"); //Imprimimos el enunciado para el usuario.
 
-//Imprimimos el enunciado para el usuario.
-	printf ("\nResultado del Producto Cruz: \n");
-
-//Con un for loop, imprimimos las componentes del nuevo Vector.
-
-	for (int i = 0; i < 3; i++)
+	for (int i = 0; i < 3; i++) //Con un for loop, imprimimos las componentes del nuevo Vector, una por una.
 	{
 		printf("%f\n", Vec3[i]);
 	}
 
-
-
-	
 return 0;
 
 }
